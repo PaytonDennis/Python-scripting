ec9ce23c80af88b51c2fc71c553b73bc9a9bb84e
PaytonDennis <102490243+PaytonDennis@users.noreply.github.com>
2025-12-12 15:11:57 -0700

    Update notes.py

--- notes.py
+++ notes.py
@@ -8,11 +8,15 @@ def tests ():
 
     text: str = "Hello World"
     list = [1, 2, 3]
-    tuple = (1, 2, 3), "duplicates, not mutable"
+    tuple = (1, 1, 1), "duplicates, not mutable"
 
     set = {1, 2, 3}, "no dublicates, mutable"
-    dict = {1: "one", 2: "two", 3: "three"};
+    dict = {1: "one", 2: "two", 3: "three"}
     "key value pairs"
     return name,int,float,str,list,tuple,set,dict
 
 print(*tests(), sep="\n")
+
+#ternary
+i = 7
+print("i should be greater than 5" if i > 5 else "i should be less than 5")
